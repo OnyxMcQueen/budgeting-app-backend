32dd7fdf8c251d67063e812e2e0008b654075281
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-07-09 15:00:06 -0400

    app.js and server.js files linked, skeleton for both files created

---
+++ app.js
@@ -0,0 +1,30 @@
+//DEPENDENCIES
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+
+//CONFIGURATION
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+
+//ROUTE
+
+app.get('/', (req, res) => {
+    res.send("Welcome to your personal budgeting app!")
+})
+
+
+
+
+
+app.get('*', (req, res) => {
+    res.status(404).send("Error: Page not found :(")
+})
+
+// EXPORT
+module.exports = app;

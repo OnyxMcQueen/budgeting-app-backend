a828796704cb0db91b459a00f1047e337b83caaa
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-07-09 15:53:17 -0400

    Created a data model

---
+++ Model/budgetModel.js
@@ -0,0 +1,59 @@
+module.exports = [
+    {
+        id: 0,
+        item_name: "Income",
+        amount: 2500,
+        date: "August 23",
+        from: "Employer",
+        category: "Income"
+    },
+    {
+        id: 1,
+        item_name: "Taxes",
+        amount: -375,
+        date: "August 30",
+        from: "Government",
+        category: "Taxes"
+    },
+    {
+        id: 2,
+        item_name: "Savings",
+        amount: -250,
+        date: "September 4",
+        from: "Bank",
+        category: "Savings"
+    },
+    {
+        id: 3,
+        item_name: "Gift",
+        amount: 150,
+        date: "September 12",
+        from: "Uncle Bob",
+        category: "Gift"
+    },
+    {
+        id: 4,
+        item_name: "Electric Bill",
+        amount: 100,
+        date: "September 18",
+        from: "Electric Compnay",
+        category: "Bills"
+    },
+    {
+        id: 5,
+        item_name: "Pet Food",
+        amount: -10,
+        date: "September 19",
+        from: "Pet Food Store",
+        category: "Pets"
+    },
+    {
+        id: 6,
+        item_name: "Coffee",
+        amount: -5,
+        date: "September 20",
+        from: "Coffee Shop",
+        category: "Food"
+    }
+
+]
